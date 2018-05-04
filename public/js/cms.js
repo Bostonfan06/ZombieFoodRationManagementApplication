13807e9a7f9cfa3c8397f423a9bb0f6c30c7561b
PsychoChris89 <christopher.tran18@gmail.com>
2018-05-03 18:15:18 -0700

    created survivor-manager.html and inventory.html

--- public/js/cms.js
+++ public/js/cms.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
-  // Getting jQuery references to the post body, title, form, and author select
+  // Getting jQuery references to the post body, title, form, and survivor select
   var bodyInput = $("#body");
   var titleInput = $("#title");
-  var cmsForm = $("#cms");
-  var authorSelect = $("#author");
+  var imsForm = $("#ims");
+  var survivorSelect = $("#survivor");
   // Adding an event listener for when the form is submitted
-  $(cmsForm).on("submit", handleFormSubmit);
+  $(imsForm).on("submit", handleFormSubmit);
   // Gets the part of the url that comes after the "?" (which we have if we're updating a post)
   var url = window.location.search;
-  var postId;
-  var authorId;
+  var itemId;
+  var survivorId;
   // Sets a flag for whether or not we're updating a post to be false initially
   var updating = false;
 
   // If we have this section in our url, we pull out the post id from the url
   // In '?post_id=1', postId is 1
-  if (url.indexOf("?post_id=") !== -1) {
-    postId = url.split("=")[1];
-    getPostData(postId, "post");
+  if (url.indexOf("?item_id=") !== -1) {
+    itemId = url.split("=")[1];
+    getItemData(itemId, "item");
   }
-  // Otherwise if we have an author_id in our url, preset the author select box to be our Author
-  else if (url.indexOf("?author_id=") !== -1) {
-    authorId = url.split("=")[1];
+  // Otherwise if we have an survivor_id in our url, preset the survivor select box to be our survivor
+  else if (url.indexOf("?survivor_id=") !== -1) {
+    survivorId = url.split("=")[1];
   }
 
-  // Getting the authors, and their posts
-  getAuthors();
+  // Getting the survivors, and their posts
+  getsurvivors();
 
   // A function for handling what happens when the form to create a new post is submitted
   function handleFormSubmit(event) {
     event.preventDefault();
-    // Wont submit the post if we are missing a body, title, or author
-    if (!titleInput.val().trim() || !bodyInput.val().trim() || !authorSelect.val()) {
+    // Wont submit the post if we are missing a body, title, or survivor
+    if (!titleInput.val().trim() || !bodyInput.val().trim() || !survivorSelect.val()) {
       return;
     }
     // Constructing a newPost object to hand to the database
-    var newPost = {
+    var newItem = {
       title: titleInput
         .val()
         .trim(),
       body: bodyInput
         .val()
         .trim(),
-      AuthorId: authorSelect.val()
+      survivorId: survivorSelect.val()
     };
 
     // If we're updating a post run updatePost to update a post
     // Otherwise run submitPost to create a whole new post
     if (updating) {
-      newPost.id = postId;
-      updatePost(newPost);
+      newItem.id = itemId;
+      updateItem(newItem);
     }
     else {
-      submitPost(newPost);
+      submitItem(newItem);
     }
   }
 
@@ -63,26 +63,26 @@ $(document).ready(function() {
     });
   }
 
-  // Gets post data for the current post if we're editing, or if we're adding to an author's existing posts
+  // Gets post data for the current post if we're editing, or if we're adding to an survivor's existing posts
   function getPostData(id, type) {
     var queryUrl;
     switch (type) {
-    case "post":
-      queryUrl = "/api/posts/" + id;
+    case "item":
+      queryUrl = "/api/items/" + id;
       break;
-    case "author":
-      queryUrl = "/api/authors/" + id;
+    case "surivor":
+      queryUrl = "/api/survivors/" + id;
       break;
     default:
       return;
     }
     $.get(queryUrl, function(data) {
       if (data) {
-        console.log(data.AuthorId || data.id);
+        console.log(data.survivorId || data.id);
         // If this post exists, prefill our cms forms with its data
         titleInput.val(data.title);
         bodyInput.val(data.body);
-        authorId = data.AuthorId || data.id;
+        SurvivorId = data.SurvivorId || data.id;
         // If we have a post with this id, set a flag for us to know to update the post
         // when we hit submit
         updating = true;
@@ -90,33 +90,33 @@ $(document).ready(function() {
     });
   }
 
-  // A function to get Authors and then render our list of Authors
-  function getAuthors() {
-    $.get("/api/authors", renderAuthorList);
+  // A function to get survivors and then render our list of survivors
+  function getSurvivors() {
+    $.get("/api/survivors", rendersurvivorList);
   }
-  // Function to either render a list of authors, or if there are none, direct the user to the page
-  // to create an author first
-  function renderAuthorList(data) {
+  // Function to either render a list of survivors, or if there are none, direct the user to the page
+  // to create an survivor first
+  function renderSurvivorsList(data) {
     if (!data.length) {
-      window.location.href = "/authors";
+      window.location.href = "/survivors";
     }
     $(".hidden").removeClass("hidden");
     var rowsToAdd = [];
     for (var i = 0; i < data.length; i++) {
-      rowsToAdd.push(createAuthorRow(data[i]));
+      rowsToAdd.push(createSurvivorRow(data[i]));
     }
-    authorSelect.empty();
+    survivorSelect.empty();
     console.log(rowsToAdd);
-    console.log(authorSelect);
-    authorSelect.append(rowsToAdd);
-    authorSelect.val(authorId);
+    console.log(survivorSelect);
+    survivorSelect.append(rowsToAdd);
+    survivorSelect.val(survivorId);
   }
 
-  // Creates the author options in the dropdown
-  function createAuthorRow(author) {
+  // Creates the survivor options in the dropdown
+  function createsurvivorRow(survivor) {
     var listOption = $("<option>");
-    listOption.attr("value", author.id);
-    listOption.text(author.name);
+    listOption.attr("value", survivor.id);
+    listOption.text(survivor.name);
     return listOption;
   }
 
