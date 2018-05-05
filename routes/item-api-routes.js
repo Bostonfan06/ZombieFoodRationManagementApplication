b18155faca7b8fe934f8851fb24eebaf1aa166ac
Lance Lee <lancechoelee@gmail.com>
2018-05-05 12:16:21 -0700

    my first iteration of the get all method for Inventory

--- routes/item-api-routes.js
+++ routes/item-api-routes.js
@@ -13,16 +13,11 @@ var db = require('../models')
 module.exports = function (app) {
   // GET route for getting all of the posts
   app.get('/api/items', function (req, res) {
-    var query = {}
-    if (req.query.survivorId) {
-      query.SurvivorId = req.query.survivorId
-    }
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Author
     db.Item.findAll({
-      where: query,
-      include: [db.Survivor]
+
     }).then(function (dbItem) {
       res.json(dbItem)
     })
--- seeds.sql
+++ seeds.sql
@@ -1,10 +1,10 @@
--- -- Some Dummy Data to fill out database
--- USE Zombie_db;
+-- Some Dummy Data to fill out database
+USE Zombie_db;
 
--- INSERT INTO Survivors (Firstname, Lastname) VALUES ('Rick', 'Grimes');
--- INSERT INTO Survivors (Firstname, Lastname) VALUES ('Carl', 'Grimes');
--- INSERT INTO Survivors (Firstname, Lastname) VALUES ('Glenn', 'Rhee');
+INSERT INTO Survivors (SurvivorId, Firstname, Lastname, createdAt, updatedAt) VALUES (0, 'Rick', 'Grimes', '2018-05-05 15:45:31', '2018-05-05 15:45:31');
+INSERT INTO Survivors (SurvivorId, Firstname, Lastname, createdAt, updatedAt) VALUES (1, 'Carl', 'Grimes', '2018-05-05 15:45:31', '2018-05-05 15:45:31');
+INSERT INTO Survivors (SurvivorId, Firstname, Lastname, createdAt, updatedAt) VALUES (2, 'Glenn', 'Rhee', '2018-05-05 15:45:31', '2018-05-05 15:45:31');
             
--- INSERT INTO Items (ItemName, Category, Units, ExpirationDate, SurvivorId) VALUES ('Canned Tuna', 'Proteins', 12, '2020-01-29', 1);
--- INSERT INTO Items (ItemName, Category, Units, ExpirationDate, SurvivorId) VALUES ('Raisin Bran', 'Grains', 2, '2019-06-18', 1);
--- INSERT INTO Items (ItemName, Category, Units, ExpirationDate, SurvivorId) VALUES ('Twinkie', 'Misc', 4, '2070-12-25', 2);
+INSERT INTO Items (ItemId, ItemName, Category, Units, ExpirationDate, SurvivorId, createdAt, updatedAt) VALUES (0, 'Canned Tuna', 'Proteins', 12, '2020-01-29', 1, '2018-05-05 15:45:31', '2018-05-05 15:45:31');
+INSERT INTO Items (ItemId, ItemName, Category, Units, ExpirationDate, SurvivorId, createdAt, updatedAt) VALUES (1, 'Raisin Bran', 'Grains', 2, '2019-06-18', 1, '2018-05-05 15:45:31', '2018-05-05 15:45:31');
+INSERT INTO Items (ItemId, ItemName, Category, Units, ExpirationDate, SurvivorId, createdAt, updatedAt) VALUES (2, 'Twinkie', 'Misc', 4, '2070-12-25', 2, '2018-05-05 15:45:31', '2018-05-05 15:45:31');
