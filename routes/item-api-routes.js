751c6c93d9341e5e83d5447967fbcddfcebf6d78
Lance Lee <lancechoelee@gmail.com>
2018-05-08 05:38:06 -0700

    hooked up belongs to functionality

--- routes/item-api-routes.js
+++ routes/item-api-routes.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Author
     db.Item.findAll({
-
+      include: [db.Survivor]
     }).then(function (dbItem) {
       res.json(dbItem)
     })
