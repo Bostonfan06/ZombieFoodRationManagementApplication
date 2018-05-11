3ff26742b63c51d5a54b9befde5da49e582092d4
Lance Lee <lancechoelee@gmail.com>
2018-05-10 18:45:01 -0700

    big change, added seans index items, refactored survivor ja

--- public/js/survivor.js
+++ public/js/survivor.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
   var lastName = $('#Lastname')
   var firstName = $('#Firstname')
   // Adding listeners to form to create an object and button to delete object
-  var audio = new Audio('../assets/audio/zomAudio2.wav');
-  audio.play();
+  var audio = new Audio('../assets/audio/zomAudio2.wav')
+  audio.play()
 
   $('#createSurvivor').on('click', function handleFormSubmit (event) {
     event.preventDefault()
@@ -19,76 +19,45 @@ $(document).ready(function () {
       firstName: firstName.val().trim(),
       lastName: lastName.val().trim()
     }
-    console.log(newSurvivor)
-    /// ///////////
-    // you have to pass the newSurvivor or else
-    submitPost(newSurvivor)
+    postSurvivor(newSurvivor)
   })
 
-  function submitPost (Survivor) {
-    /// ////////////
-    // or else over here Survivor has no value
+  function postSurvivor (Survivor) {
     $.post('/api/survivor', Survivor, function () {
       console.log('posted')
+    }).then(function () {
+      $('#Firstname').empty()
+      $('#Lastname').empty()
+      location.reload()
     })
   }
 
-  ///////////////////////////////////////
   function getSurvivors () {
     $.get('/api/survivor', function (data) {
       for (var i = 0; i < data.length; i++) {
-        var row = createNewRow2(data[i], i)
+        var row = createSurvivorRow(data[i], i)
         $('#survivorBody').append(row)
       }
     })
   }
   getSurvivors()
 
-  function createNewRow2 (survivor, index) {
-    var row = '<tr>' +
+  function createSurvivorRow (survivor, index) {
+    var survivorRow = '<tr>' +
       '<th scope="row">' + (index + 1) + '</th>' +
-      '<td>' + survivor.SurvivorID + '</td>' +
       '<td>' + survivor.FirstName + '</td>' +
       '<td>' + survivor.LastName + '</td>' +
       '<td>' + survivor.SurvivorItems + '</td>' +
+      '<td>' + survivor.SurvivorItems + '</td>' +
     '</tr>'
 
-    return row
+    return survivorRow
   }
-  ////////////////////////////////////////
-  // //Grabbing list of Survivors
-  // getSurvivors()
 
-  // //Function that handles what happens when the form is submitted to create a new survivor
-  // function handleSurvivorFormSubmit (event) {
-  // event.preventDefault()
-  // // Don't do anything if the name fields hasn't been filled out
-  // if (!nameInput.val().trim().trim()) {
-  //   return
-  // }
-  // // Calling upsertSurvivor function and passing in the value of the name input
-  // upsertSurvivor({
-  //   name: nameInput.val().trim()
-  // })
-  // }
-  // Function for creating a new list of row for Survivor
   function upsertSurvivor (survivorData) {
     $.post('/api/survivor', survivorData).then(getSurvivors)
   }
 
-  function createSurvivorRow (survivorData) {
-    var newTr = $('<tr>')
-    newTr.data('survivor', survivorData)
-    newTr.append('<td>' + survivorData.name + '</td>')
-    newTr.append('<td>' + survivorData.Posts.length + '</td>')
-    newTr.append("<td><a href='/inventory?survior_id=" + survivorData.id + "'>Go to Posts</a></td>")
-    newTr.append("<td><a href='/ims?survivor_id=" + survivorData.id + "'>Create a Post</a></td>")
-    newTr.append("<td><a style='cursor:pointer; color:red' class='delete-survivor'>Delete Survivor</a></td>")
-
-    return newTr
-  }
-
- 
   function renderSurvivorList (rows) {
     survivorList.children().not(':last').remove()
     survivorContainer.children('.alert').remove()
